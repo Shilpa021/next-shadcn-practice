173ba6ed4c175bbdee0761097facf213a849a40c
Shilpa021 <rana.shilpa021@gmail.com>
2026-03-12 19:32:17 +0530

    feat: initial commit

---
+++ src/app/practice/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function page() {
+  return (
+    <main className='h-full flex justify-center items-center flex-col'>
+ <div>
+      We are here 
+    </div>
+    <button className='px-6 py-2 bg-blue-400 rounded'>
+        Test Button
+    </button>
+    </main>
+   
+  )
+}
+
+export default page
